51abf459d174bda486c265421091771184a5d638
Alejandro Castro <soyracm@gmail.com>
2025-07-22 21:14:15 -0400

    prisma

--- projects/01-myfirstapp/src/users/users.service.ts
+++ projects/01-myfirstapp/src/users/users.service.ts
@@ -1,31 +1,20 @@
-
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UsersService {
-    private users: any[] = [
-        {
-            id:1,
-            name: 'Jhon Doe',
-            phone: '123456789'
-        },
-        {
-            id:2,
-            name: 'Jane Doe',
-            phone: '987654321'
-        }
-    ]
-    
+   
+    constructor(private prisma: PrismaService) {}
+   
     getUsers() {
-        return this.users
+        //Consulta de base de datos
+        return this.prisma.user.findMany();
     }
 
+
     createUser (user: CreateUserDto) {
-        this.users.push(user)
-        return {
-            ...user,
-            id: this.users.length + 1
-        }
+        return this.prisma.user.create({data: user});
     }
 }
+
