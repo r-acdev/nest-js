e7fe1021b63d8e7d71aedc3536c1c2a9adb1f101
Alejandro Castro <soyracm@gmail.com>
2025-07-22 21:32:54 -0400

    swagger

--- projects/01-myfirstapp/src/main.ts
+++ projects/01-myfirstapp/src/main.ts
@@ -1,15 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-// El whitelist es para que no me envie un campo de mas
-
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }))
+
+  //Pones el codigo aqui
+
+   const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  //------------------------------------------------
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
